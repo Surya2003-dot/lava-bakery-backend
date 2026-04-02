3ae10fed252f4296514e6c9a2415fd678ed7ca6c
Surya2003-dot <suryavish2003@gmail.com>
2026-04-02 23:43:26 +0530

    cros

--- BackEnd/bakery/src/main/java/com/lava/bakery/config/SecurityConfig.java
+++ BackEnd/bakery/src/main/java/com/lava/bakery/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
