936226137964c2c7a55c07d60a780ed48103846c
Surya2003-dot <suryavish2003@gmail.com>
2026-04-02 21:49:59 +0530

    url changes

--- BackEnd/bakery/src/main/java/com/lava/bakery/config/SecurityConfig.java
+++ BackEnd/bakery/src/main/java/com/lava/bakery/config/SecurityConfig.java
@@ -68,12 +68,8 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-//        config.setAllowedOriginPatterns(List.of("https://lava-bakery-backend.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://lava-bakery-backend.vercel.app", "http://localhost:3000"));
 
-        config.setAllowedOriginPatterns(List.of(
-                "http://localhost:3000",
-                "https://lava-bakery.vercel.app"
-        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         config.setAllowedHeaders(List.of("*"));
