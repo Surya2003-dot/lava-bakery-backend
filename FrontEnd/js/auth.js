2fe523d382ed94a5e9a1ceeab9b89c35e4b2cec4
Surya2003-dot <suryavish2003@gmail.com>
2026-04-03 15:44:23 +0530

    Fix:Login Issuse

--- FrontEnd/js/auth.js
+++ FrontEnd/js/auth.js
@@ -68,7 +68,7 @@ emailError.style.display="block";
 return;
 }
 
- fetch("https://lava-bakery-backend.onrender.com/api/auth/send-register-otp",{
+fetch(`${API_BASE_URL}/auth/send-register-otp`,{
 method:"POST",
 
 headers:{
