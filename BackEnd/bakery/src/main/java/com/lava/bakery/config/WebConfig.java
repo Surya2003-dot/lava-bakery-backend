aaa2e680aa9c68bb6a4b6b37ac53eb780a20f04e
Surya2003-dot <suryavish2003@gmail.com>
2026-04-04 11:29:02 +0530

    changes in webConfig code

--- BackEnd/bakery/src/main/java/com/lava/bakery/config/WebConfig.java
+++ BackEnd/bakery/src/main/java/com/lava/bakery/config/WebConfig.java
@@ -1,17 +1,17 @@
-package com.lava.bakery.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+//package com.lava.bakery.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
 //
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/uploads/**")
 //           .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
 //                .addResourceLocations("file:/app/uploads/");
-    }
-
-}
+//    }
+//
+//}
