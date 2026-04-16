005b18f769391d8050803a2940128d872c2b85d6
Surya2003-dot <suryavish2003@gmail.com>
2026-04-16 20:54:15 +0530

    encoding removed for msg

--- BackEnd/bakery/src/main/java/com/lava/bakery/service/TelegramService.java
+++ BackEnd/bakery/src/main/java/com/lava/bakery/service/TelegramService.java
@@ -3,8 +3,8 @@ package com.lava.bakery.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TelegramService {
@@ -16,16 +16,15 @@ public class TelegramService {
 
         try{
 
-            String encodedMsg = URLEncoder.encode(text, StandardCharsets.UTF_8);
-
-            String url = "https://api.telegram.org/bot" + BOT_TOKEN
-                    + "/sendMessage?chat_id=" + CHAT_ID
-                    + "&text=" + encodedMsg;
-
-            System.out.println("TELEGRAM URL: " + url);
+            String url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage";
 
             RestTemplate rest = new RestTemplate();
-            rest.getForObject(url, String.class);
+
+            Map<String, String> body = new HashMap<>();
+            body.put("chat_id", CHAT_ID);
+            body.put("text", text);
+
+            rest.postForObject(url, body, String.class);
 
         }catch(Exception e){
             e.printStackTrace();
